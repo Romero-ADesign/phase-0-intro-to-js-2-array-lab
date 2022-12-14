cadb519620b9ada04dbf4103c3cce27ffa23edc6
Adam Romero <aromero0904@gmail.com>
2022-12-14 10:51:03 -0500

    Deleted Everything

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat() {
+    const coolCats = [...cats, "Broom"]
+    return coolCats
+} 
+
+function prependCat() {
+    const coolCats = ["Arnold", ...cats]
+    return coolCats
+} 
+
+function removeLastCat() {
+    const coolCats = [...cats.slice(0, 2)]
+    return coolCats
+}
+
+function removeFirstCat() {
+    const coolCats = [...cats.slice(1)]
+    return coolCats
+}
